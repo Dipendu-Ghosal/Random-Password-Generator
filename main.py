6b6e193c870ead5a3f203bcdf27e64756efc1e9d
Dipendu Ghosal <dipendughosal@gmail.com>
2025-09-09 17:18:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import string
 
 password_len = 12
 charValues = string.ascii_letters + string.digits + string.punctuation
-password = ""
-for i in range(password_len):
-    password += random.choice(charValues)
+# password = ""
+# for i in range(password_len):
+#     password += random.choice(charValues)
+password = "".join([random.choice(charValues) for i in range (password_len)])
 
 print(f"Your password is - {password}")
