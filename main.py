159af470a883ae029d71bcbd6d406d76cb885689
Dipendu Ghosal <dipendughosal@gmail.com>
2025-09-09 17:11:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import random
 import string
 
+password_len = 12
 charValues = string.ascii_letters + string.digits + string.punctuation
+password = ""
+for i in range(password_len):
+    password += random.choice(charValues)
 
-print(random.choice(charValues))
+print(f"Your password is - {password}")
